6d3c973839e6c3191d59a1fcb910240cd777bc86
amitghoshanto <amitanto94@gmail.com>
2023-11-19 19:51:19 +0600

    User Data Update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -13,5 +14,41 @@ class UserController extends Controller
             'title' => 'Profile',
         ];
         return view('profile.index', compact('meta'));
+    }
+
+    public function edit(Request $request)
+    {
+        $rules = [
+            'first_name' => ['required'],
+            'last_name' => ['required'],
+            'gender' => ['required', 'numeric'],
+            'dob_d' => ['required', 'numeric'],
+            'dob_m' => ['required', 'numeric'],
+            'dob_y' => ['required', 'numeric'],
+            'email' => ['required', 'email'],
+
+        ];
+        $custommessage = [
+            'first_name' => 'Insert First Name',
+            'last_name' => 'Insert Last Name',
+            'gender' => 'Select Gender',
+            'dob_d' => 'Select Day',
+            'dob_m' => 'Select Month',
+            'dob_y' => 'Select Year',
+            'email' => 'Insert Email Address',
+
+        ];
+        $this->validate($request, $rules, $custommessage);
+
+        User::where('id', Auth::user()->id)->update([
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'gender' => $request->gender,
+            'dob_d' => $request->dob_d,
+            'dob_m' => $request->dob_m,
+            'dob_y' => $request->dob_y,
+            'email' => $request->email,
+        ]);
+        return back()->with(['alert' => 'success', 'title' => 'Profile Updated', 'muted' => 'Profile Updated.']);
     }
 }
