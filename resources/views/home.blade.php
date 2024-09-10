989282737ae80b34bedabfe8ff403df5f56378b5
Amit Ghosh Anto <amitanto94@gmail.com>
2024-09-10 18:12:40 +0600

    Home Page Change

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,20 @@
 <x-layouts.main :meta="$meta">
 
 
-    <div class="container container-tight pt-4 text-center">
-        <img src="https://getsetrock.com/site/img/events/legends-of-the-92/t2iuEW8SkQGqziOm42ny.jpg" alt=""
-            class="img-fluid w-100">
+    <div class="container pt-4 text-center">
+
+        <div class="row">
+            <div class="col-md-12">
+                @if (isMobile())
+                    <img src="https://getsetrock.com/site/img/events/legends-of-the-92/t2iuEW8SkQGqziOm42ny.jpg"
+                        alt="" class="img-fluid w-100">
+                @else
+                    <img src="https://getsetrock.com/site/img/events/legends-of-the-92/GC0vfWGwS8kgKNth5v5D.jpg"
+                        alt="" class="img-fluid w-100">
+                @endif
+
+            </div>
+        </div>
 
 
 
